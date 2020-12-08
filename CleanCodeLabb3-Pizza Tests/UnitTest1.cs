f490d40f0e2c92905297dbe0412b6770b4d5f656
Luna <Hanken95@gmail.com>
2020-12-08 14:35:24 +0100

    Created Testproject

---
+++ CleanCodeLabb3-Pizza Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CleanCodeLabb3_Pizza_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
