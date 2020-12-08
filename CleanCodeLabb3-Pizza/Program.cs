449d29ac1365f58adea340bac30ed274186707ad
Luna <Hanken95@gmail.com>
2020-12-08 14:34:34 +0100

    Created solution and project

---
+++ CleanCodeLabb3-Pizza/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CleanCodeLabb3_Pizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
